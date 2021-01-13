39c1f7ed22dbf8f84e2e2f4b75cdc9ebee4553d2
amoghnaik-hmcts <70205551+amoghnaik-hmcts@users.noreply.github.com>
2021-01-13 09:36:24 +0000

    added CCDCaseType.csv

--- src/gatling/simulations/uk/gov/hmcts/reform/LRD/performance/scenarios/GetScenario.scala
+++ src/gatling/simulations/uk/gov/hmcts/reform/LRD/performance/scenarios/GetScenario.scala
@@ -7,23 +7,25 @@ import uk.gov.hmcts.reform.LRD.performance.scenarios.utils.Environment
 object GetScenario {
 
   val feederFile = csv("OrgIDStructure.csv").circular
+  val feederFile2 = csv("CCDCaseType.csv").circular
 
   val GetScenario = scenario("GetScenario")
-    .feed(feederFile)
     .exec(http(requestName="LRD_010_NoServiceCode")
       .get("/refdata/location/orgServices")
       .headers(Environment.headers_1)
       .check(status.is(200)))
     .pause(2)
 
+    .feed(feederFile)
     .exec(http(requestName="LRD_020_ServiceCode")
       .get("/refdata/location/orgServices?service_code=${Service_Code}")
       .headers(Environment.headers_1)
       .check(status.is(200)))
     .pause(2)
 
+    .feed(feederFile2)
     .exec(http(requestName="LRD_030_CCDCaseType")
-      .get("/refdata/location/orgServices?service_code=MoneyClaimCase")
+      .get("/refdata/location/orgServices?service_code=${CCD_CASE_TYPE}")
       .headers(Environment.headers_1)
       .check(status.is(200)))
     .pause(2)
