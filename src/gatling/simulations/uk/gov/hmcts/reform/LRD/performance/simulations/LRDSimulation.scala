b34c053f3bedeebf5045d7a963f4921a89e349f4
amoghnaik-hmcts <70205551+amoghnaik-hmcts@users.noreply.github.com>
2021-01-18 10:34:41 +0000

    Update LRDSimulation.scala

--- src/gatling/simulations/uk/gov/hmcts/reform/LRD/performance/simulations/LRDSimulation.scala
+++ src/gatling/simulations/uk/gov/hmcts/reform/LRD/performance/simulations/LRDSimulation.scala
@@ -7,27 +7,18 @@ import uk.gov.hmcts.reform.LRD.performance.scenarios._
 import scala.concurrent.duration._
 
 class LRDSimulation extends Simulation{
-  
-  val rampUpDurationMins = 2
-  val rampDownDurationMins = 2
-  val testDurationMins = 60
-  val HourlyTarget:Double = 600
-  val RatePerSec = HourlyTarget / 3600
 
   val httpProtocol = http
   //.proxy(Proxy("proxyout.reform.hmcts.net", 8080).httpsPort(8080))
     .baseUrl(Environment.baseURL)
 
-  val LRDScenario = scenario("RoleAssignmentScenario").repeat(1)
+  val LRDScenario = scenario("RoleAssignmentScenario")
+  .exec(IDAMHelper.getIdamToken)
+  .exec(S2SHelper.S2SAuthToken)
+  .repeat(120)
     {
-        exec(IDAMHelper.getIdamToken)
-        .exec(S2SHelper.S2SAuthToken)
-        .exec(GetScenario.GetScenario)
+        exec(GetScenario.GetScenario)
     }
 
-  setUp(LRDScenario.inject(
-      rampUsersPerSec(0.00) to (RatePerSec) during (rampUpDurationMins minutes),
-      constantUsersPerSec(RatePerSec) during (testDurationMins minutes),
-      rampUsersPerSec(RatePerSec) to (0.00) during (rampDownDurationMins minutes)
-    )).protocols(httpProtocol)
+  setUp(LRDScenario.inject(rampUsers(10) during(600))).protocols(httpProtocol)
 }
